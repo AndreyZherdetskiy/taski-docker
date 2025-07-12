63c042501abed05797f164fd94b704eb9d7bb9b2
Andrey Zherdetskiy <aszherdetskiy@gmail.com>
2025-07-12 16:17:48 +0400

    Add description field

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,13 +1,16 @@
 const Task = ({ data, handleEdit, handleDelete }) => {
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
+    <li className="list-group-item d-flex justify-content-between align-items-center flex-column align-items-start">
       <span
         className={`task-title mr-2 ${data.completed ? "completed-task" : ""}`}
         title={data.description}
       >
         {data.title}
       </span>
-      <span>
+      <span className="text-muted small w-100" style={{ wordBreak: 'break-word' }}>
+        {data.description}
+      </span>
+      <span className="align-self-end mt-2">
         <button
           className="btn btn-secondary mr-2"
           onClick={() => handleEdit(data)}
